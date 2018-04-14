e75861e46d8566d11e769f1dd0f3364b60a7bafb
Yevgen Voronenko <yevgen@reflexionmedical.com>
2018-04-14 02:09:28 -0700

    do not use MATLAB eval() to parse arrays that contain quote marks ("), this leads to bugs in new version of Matlab + fix indentaion in code

--- loadjson.m
+++ loadjson.m
@@ -155,7 +155,7 @@ global pos inStr isoct
     end
 
     if next_char ~= ']'
-	if(jsonopt('FastArrayParser',1,varargin{:})>=1 && arraydepth>=jsonopt('FastArrayParser',1,varargin{:}))
+        if(jsonopt('FastArrayParser',1,varargin{:})>=1 && arraydepth>=jsonopt('FastArrayParser',1,varargin{:}))
             [endpos, e1l, e1r]=matching_bracket(inStr,pos);
             arraystr=['[' inStr(pos:endpos)];
             arraystr=regexprep(arraystr,'"_NaN_"','NaN');
@@ -163,34 +163,35 @@ global pos inStr isoct
             arraystr(arraystr==sprintf('\n'))=[];
             arraystr(arraystr==sprintf('\r'))=[];
             %arraystr=regexprep(arraystr,'\s*,',','); % this is slow,sometimes needed
-            if(~isempty(e1l) && ~isempty(e1r)) % the array is in 2D or higher D
-        	astr=inStr((e1l+1):(e1r-1));
-        	astr=regexprep(astr,'"_NaN_"','NaN');
-        	astr=regexprep(astr,'"([-+]*)_Inf_"','$1Inf');
-        	astr(astr==sprintf('\n'))=[];
-        	astr(astr==sprintf('\r'))=[];
-        	astr(astr==' ')='';
-        	if(isempty(find(astr=='[', 1))) % array is 2D
+            if (~isempty(e1l) && ~isempty(e1r)) % the array is in 2D or higher D
+                astr=inStr((e1l+1):(e1r-1));
+                astr=regexprep(astr,'"_NaN_"','NaN');
+                astr=regexprep(astr,'"([-+]*)_Inf_"','$1Inf');
+                astr(astr==sprintf('\n'))=[];
+                astr(astr==sprintf('\r'))=[];
+                astr(astr==' ')='';
+                if(isempty(find(astr=='[', 1))) % array is 2D
                     dim2=length(sscanf(astr,'%f,',[1 inf]));
-        	end
+                end
             else % array is 1D
-        	astr=arraystr(2:end-1);
-        	astr(astr==' ')='';
-        	[obj, count, errmsg, nextidx]=sscanf(astr,'%f,',[1,inf]);
-        	if(nextidx>=length(astr)-1)
+                astr=arraystr(2:end-1);
+                astr(astr==' ')='';
+                [obj, count, errmsg, nextidx]=sscanf(astr,'%f,',[1,inf]);
+                if(nextidx>=length(astr)-1)
                     object=obj;
                     pos=endpos;
                     parse_char(']');
                     return;
-        	end
+                end
             end
+            
             if(~isempty(dim2))
-        	astr=arraystr;
-        	astr(astr=='[')='';
-        	astr(astr==']')='';
-        	astr(astr==' ')='';
-        	[obj, count, errmsg, nextidx]=sscanf(astr,'%f,',inf);
-        	if(nextidx>=length(astr)-1)
+                astr=arraystr;
+                astr(astr=='[')='';
+                astr(astr==']')='';
+                astr(astr==' ')='';
+                [obj, count, errmsg, nextidx]=sscanf(astr,'%f,',inf);
+                if(nextidx>=length(astr)-1)
                     if(dim2~=0)
                         object=reshape(obj,dim2,numel(obj)/dim2)';
                     end
@@ -200,30 +201,32 @@ global pos inStr isoct
                         waitbar(pos/length(inStr),pbar,'loading ...');
                     end
                     return;
-        	end
+                end
             end
             arraystr=regexprep(arraystr,'\]\s*,','];');
-	else
+        else
             arraystr='[';
-	end
+        end
+        
         try
-           if(isoct && regexp(arraystr,'"','once'))
+            if(regexp(arraystr,'"','once'))
                 error('Octave eval can produce empty cells for JSON-like input');
-           end
-           object=eval(arraystr);
-           pos=endpos;
-        catch
-         while 1
-            newopt=varargin2struct(varargin{:},'JSONLAB_ArrayDepth_',arraydepth+1);
-            val = parse_value(newopt);
-            object{end+1} = val;
-            if next_char == ']'
-                break;
             end
-            parse_char(',');
-         end
+            object=eval(arraystr);
+            pos=endpos;
+        catch
+            while 1
+                newopt=varargin2struct(varargin{:},'JSONLAB_ArrayDepth_',arraydepth+1);
+                val = parse_value(newopt);
+                object{end+1} = val;
+                if next_char == ']'
+                    break;
+                end
+                parse_char(',');
+            end
         end
     end
+    
     if(jsonopt('SimplifyCell',0,varargin{:})==1)
       try
         oldobj=object;
